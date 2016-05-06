4f637f8deb43f9419c0fdb80d32297c1fe8a7c0e
4everinbeta <4everinbeta@gmail.com>
2016-05-06 13:41:35 -0600

    Changing to use local ssh key

--- monitor.py
+++ monitor.py
@@ -6,17 +6,15 @@ from alarmageddon.validations.ssh import SshContext
 from alarmageddon.publishing.hipchat import HipChatPublisher
 
 # Setup ssh context
-ssh_ctx = SshContext("ec2-user","/Users/Ryan/.ssh/kafka-key.pem")
+ssh_ctx = SshContext("ec2-user","/home/ec2-user/.ssh/kafka-key.pem")
 
 # Define kafka Validation
 zk_nodes = '10.196.2.40:2181,10.196.2.39:2181,10.196.2.38:2181'
 kafka_hosts = ['52.26.246.89','52.24.9.167','52.27.168.120']
-# kafka_validation = KafkaStatusValidation(ssh_ctx, zookeeper_nodes=zk_nodes,hosts=kafka_hosts,kafka_list_topic_command="/usr/local/kafka/bin/kafka-topics.sh --describe")
 
 kafka_offset_monitor = KafkaConsumerLagMonitor(ssh_ctx, zookeeper_nodes=zk_nodes,hosts=kafka_hosts)
 
 validations = []
-# validations.append(kafka_validation)
 validations.append(kafka_offset_monitor)
 
 # Define publishers
